cc5076f7a8590e87704676f79c2a6f9d91f4ab2f
Long Yang <Loyang@Long-Yangs-MacBook-Pro.local>
2017-09-23 23:37:23 +0200

    test 03

--- pages/index/index.js
+++ pages/index/index.js
@@ -7,7 +7,7 @@ Page({
     motto: 'Hello World',
     userInfo: {},
     hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    
   },
   //事件处理函数
   bindViewTap: function() {
@@ -44,6 +44,7 @@ Page({
     }
   },
   getUserInfo: function(e) {
+    console.log(e)
     app.globalData.userInfo = e.detail.userInfo
     this.setData({
       userInfo: e.detail.userInfo,
