20191c9516c39b71b3c8d68c622bf8af90c6f714
Yang <Long.Yang@t-systems.com>
2017-09-26 17:25:13 +0200

    test 26.09

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,12 +4,14 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    
+    imgUrls: [
+      'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+    ]   
   },
   //事件处理函数
+  /*
   bindViewTap: function() {
     wx.navigateTo({
       url: '../logs/logs'
@@ -50,5 +52,5 @@ Page({
       userInfo: e.detail.userInfo,
       hasUserInfo: true
     })
-  }
+  }*/
 })
