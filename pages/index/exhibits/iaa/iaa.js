5baeca72ca5f42dd2dcf8a745f961c1371854213
Long Yang <Loyang@Long-Yangs-MacBook-Pro.local>
2017-10-09 23:38:23 +0200

    091017

---
+++ pages/index/exhibits/iaa/iaa.js
@@ -0,0 +1,11 @@
+Page({
+    data:{
+        images:[
+               "images/iaa01.jpg",
+               "images/iaa02.jpg",
+               "images/iaa03.jpg",
+               "images/iaa04.jpg",
+        ]
+    }
+    
+})
